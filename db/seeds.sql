8e9a00e48099b9b3b6041711389f489635e54792
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-11-18 17:34:14 -0500

    added new burgers as seeds for db

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers(burger_name, devoured, date)
+
+VALUES ("Double Cheeseburger", false, "2019-11-16 04:20:00"),
+  ("Cheeseburger", false, "2019-11-16 04:20:01"),
+  ("Veggie Burger", false, "2019-11-16 04:20:02"),
+  ("Baconator", false, "2019-11-16 16:20:00"),
+  ("Half Pounder", false, "2019-11-16 16:20:01"),
+  ("Baby Burger", false, "2019-11-16 04:20:20"),
+  ("Double Trouble", true, "2019-11-16 16:20:16");
